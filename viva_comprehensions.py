771b74d698bd2f75a159d89bfbaf4103fd8a25e5
nathaneckel <nathan@patientpaperwork.com>
2024-03-21 18:05:37 -0400

    final commit

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -8,24 +8,32 @@ class Parity(enum.Enum):
 
 
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
-    """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    return [i for i in range(start, stop) if i % 2 != parity.value]
 
-    :param start:
+#RETURN     [  ACTION     ITERATE    (for)     CONDITION (if) ]
+
+# DO   FOR    IF
+# NOT  FOR    IF      DO  (like normally)
+
+"""
+Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+updating this here docstring to something useful.
+
+:param start:
     :param stop:
     :param parity:
     :return:
     """
-    pass
-
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
-    """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    return {i:strategy(i) for i in range(start, stop)}
+
+
+"""
+Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+updating this here docstring to something useful.
 
 
     :param start:
@@ -37,12 +45,16 @@ def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
 
 
 def gen_set(val_in: str) -> Set:
-    """
-    Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
-    what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
-    updating this here docstring to something useful.
+    return set([letter.upper() for letter in val_in if letter.islower()])
+
+#    return {i:strategy(i) for i in range(start, stop)}
+#     return [i for i in range(start, stop) if i % 2 != parity.value]
+"""
+THIS IS ALL ABOUT CAPS / lowers / Title / camelCase
+Oh no some evil developer decided not to write docstrings. Maybe you can use the test cases to decipher
+what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
+updating this here docstring to something useful.
 
     :param val_in:
     :return:
     """
-    pass
